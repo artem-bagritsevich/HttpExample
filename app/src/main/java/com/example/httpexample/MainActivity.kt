b197b78695ea50e9451c8254dd4c3c11f539a21b
Assasinnys <dmitry.zenevich@yandex.by>
2020-07-02 22:04:52 +0300

    feat: add okHttp implementation

--- app/src/main/java/com/example/httpexample/MainActivity.kt
+++ app/src/main/java/com/example/httpexample/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.example.httpexample
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Toast
 import androidx.annotation.WorkerThread
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
-import org.json.JSONObject
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-import java.net.HttpURLConnection
-import java.net.URL
+import java.util.concurrent.TimeUnit
 
 private const val ENDPOINT = "http://10.0.2.2:3000"  // Im using json-server running on my localhost and emulator
 private const val BOOKS_URI = "/books"
 private const val TITLE = "title"
 
 class MainActivity : AppCompatActivity() {
+
+    private val okHttpClient by lazy {
+        OkHttpClient().newBuilder().connectTimeout(10, TimeUnit.SECONDS).build()
+    }
+    private val typeJson = "application/json".toMediaType()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,7 +39,36 @@ class MainActivity : AppCompatActivity() {
 
     @WorkerThread
     fun getBooksAndShowIt() {
-        val httpUrlConnection = URL(ENDPOINT + BOOKS_URI).openConnection() as HttpURLConnection
+        val request: Request = Request.Builder()
+            .url(ENDPOINT + BOOKS_URI)
+            .build()
+
+        try {
+            okHttpClient.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    toast(getString(R.string.error_unsuccessful_request, response.code))
+                    return
+                }
+
+                val resultString = response.body?.string() ?: ""
+                val bookList = mutableListOf<String>()
+                val jsonBookArray = JSONArray(resultString)
+
+                for (i in 0 until jsonBookArray.length()) {
+                    val book = jsonBookArray.getJSONObject(i)
+                    bookList.add(book.getString(TITLE))
+                }
+
+                runOnUiThread {
+                    textView.text = bookList.reduce { acc, s -> "$acc\n$s" }
+                }
+            }
+        } catch (e: Exception) {
+            toast(getString(R.string.error_exception))
+            e.printStackTrace()
+        }
+
+        /*val httpUrlConnection = URL(ENDPOINT + BOOKS_URI).openConnection() as HttpURLConnection
         httpUrlConnection.apply {
             connectTimeout = 10000 // 10 seconds
             requestMethod = "GET"
@@ -61,12 +95,32 @@ class MainActivity : AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).post {
             textView.text = books.reduce { acc, s -> "$acc\n$s" }
-        }
+        }*/
     }
 
     @WorkerThread
     fun addBook(book: String) {
-        val httpUrlConnection = URL(ENDPOINT + BOOKS_URI).openConnection() as HttpURLConnection
+        val requestBody = "{\"$TITLE\": \"$book\"}".toRequestBody(contentType = typeJson)
+        val request = Request.Builder()
+            .url(ENDPOINT + BOOKS_URI)
+            .post(requestBody)
+            .build()
+
+        try {
+            okHttpClient.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    toast(getString(R.string.error_unsuccessful_request, response.code))
+                } else {
+                    toast(getString(R.string.successful_request))
+                    getBooksAndShowIt()
+                }
+            }
+        } catch (e: Exception) {
+            toast(getString(R.string.error_exception))
+            e.printStackTrace()
+        }
+
+        /*val httpUrlConnection = URL(ENDPOINT + BOOKS_URI).openConnection() as HttpURLConnection
         val body = JSONObject().apply {
             put(TITLE, book)
         }
@@ -81,6 +135,12 @@ class MainActivity : AppCompatActivity() {
         }
         httpUrlConnection.responseCode
         httpUrlConnection.disconnect()
-        getBooksAndShowIt()
+        getBooksAndShowIt()*/
+    }
+}
+
+fun AppCompatActivity.toast(text: String) {
+    runOnUiThread {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
